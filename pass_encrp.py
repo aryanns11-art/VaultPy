453d0b0a750be60028b42148b3693af466adb33f
aryanns11-art <aryanns1104@gmail.com>
2026-06-16 20:36:26 +0530

    feat : Added Generate passowrd & Login to check passwords

--- pass_encrp.py
+++ pass_encrp.py
@@ -1,5 +1,8 @@
 import json
 from urllib.parse import urlparse
+from getpass import getpass
+import random
+import string
 
 FILE_NAME = "vault.json"
 
@@ -36,7 +39,7 @@ def add_password(vault):
         print("Website already exists!")
         return
     username = input("Username: ")
-    password = input("Password: ")
+    password = getpass("Password: ")
 
     vault[website] = {
         "username": username,
@@ -90,7 +93,7 @@ def update_passwords(vault):
     if website in vault:
 
         new_username = input("New Username (Enter to keep old): ")
-        new_password = input("New Password (Enter to keep old): ")
+        new_password = getpass("New Password (Enter to keep old): ")
 
         if new_username:
             vault[website]["username"] = new_username
@@ -102,7 +105,21 @@ def update_passwords(vault):
         print("Updated Successfully")
 
     else:
-        print("Web  site not found in stored websites !")      
+        print("Web  site not found in stored websites !")   
+
+#----------------------------------------------------------------------------------------------------
+
+def generate_password(length=8):
+
+    chars = string.ascii_letters + string.digits + "!@#$%^&*"
+
+    password = "".join(
+        random.choice(chars)
+        for _ in range(length)
+    )
+
+    return password
+           
 #----------------------------------------------------------------------------------------------------
 
 def delete_password(vault):
@@ -118,42 +135,62 @@ def delete_password(vault):
 
 #----------------------------------------------------------------------------------------------------
 
+def chk_masterpass():
+
+    masterpass = getpass("Enter Master Password to View all passowrds :")
+
+    if masterpass == "Aryan9911":
+        return True
+    else:
+        print("Incorrect Password ! Access denied !")
+        return False
+
+#----------------------------------------------------------------------------------------------------
+
 vault = load_data()
 
 def main():
 
-    while True:
+    login = chk_masterpass()
 
-        print("\n1. Add Pass")
-        print("2. Show Pass")
-        print("3. Search Pass")
-        print("4. Delete Pass")
-        print("5. Update Password")
-        print("6. Exit")
+    if login:
 
-        choice = input("Enter your choice: ")
+        while True:
 
-        if choice == '1':
-            add_password(vault)
+            print("\n1. Add Pass")
+            print("2. Show Pass")
+            print("3. Search Pass")
+            print("4. Delete Pass")
+            print("5. Update Password")
+            print("6. Genearte Password")
+            print("7. Exit")
 
-        elif choice == '2':
-            view_password(vault)
+            choice = input("Enter your choice: ")
 
-        elif choice == '3':
-            search_password(vault)
+            if choice == '1':
+                add_password(vault)
 
-        elif choice == '4':
-            delete_password(vault)
+            elif choice == '2':
+                view_password(vault)
 
-        elif choice == '5':
-            update_passwords(vault)    
+            elif choice == '3':
+                search_password(vault)
 
-        elif choice == '6':
-            print("Goodbye!")
-            break
+            elif choice == '4':
+                delete_password(vault)
 
-        else:
-            print("Invalid choice!")
+            elif choice == '5':
+                update_passwords(vault)  
+
+            elif choice == '6':
+                generate_password()
+
+            elif choice == '7':
+                print("Goodbye!")
+                break
+
+            else:
+                print("Invalid choice!")
 
 if __name__ == "__main__":
     main()
